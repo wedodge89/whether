6db242acdf1e40fea7e6ba7b9cbcb855e38d4c51
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2019-11-05 19:53:26 -0500

    Converged APIS

--- assets/javascript/bored.js
+++
@@ -1,9 +0,0 @@
-var queryURL = "http://www.boredapi.com/api/activity/";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}) // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
-    console.log(response.activity);
-  });
